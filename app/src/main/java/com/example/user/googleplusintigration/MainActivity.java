5fb5688947e8ed2eba20329db8efddd4265132c7
shubham chauhan <shubham.chauhan@kelltontech.com>
2017-09-06 12:09:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/googleplusintigration/MainActivity.java
@@ -0,0 +1,169 @@
+package com.example.user.googleplusintigration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+    private SignInButton signInButton;
+
+    //Signing Options
+    private GoogleSignInOptions gso;
+
+    //google api client
+    private GoogleApiClient mGoogleApiClient;
+
+    //Signin constant to check the activity result
+    private int RC_SIGN_IN = 100;
+
+    //TextViews
+    private TextView textViewName;
+    private TextView textViewEmail;
+    private NetworkImageView profilePhoto;
+
+    //Image Loader
+    private ImageLoader imageLoader;
+    private Button btnSignOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing Views
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        textViewEmail = (TextView) findViewById(R.id.textViewEmail);
+        profilePhoto = (NetworkImageView) findViewById(R.id.profileImage);
+        btnSignOut = (Button) findViewById(R.id.btn_sign_out);
+
+        //Initializing google signin option
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        //Initializing signinbutton
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setScopes(gso.getScopeArray());
+
+        //Initializing google api client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        //Setting onclick listener to signing button
+        signInButton.setOnClickListener(this);
+        btnSignOut.setOnClickListener(this);
+    }
+
+
+    //This function will option signing intent
+    private void signIn() {
+        //Creating an intent
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+
+        //Starting intent for result
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        updateUI(true);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //If signin
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            //Calling a new function to handle signin
+            handleSignInResult(result);
+        }
+    }
+
+
+    //After the signing we are calling this function
+    private void handleSignInResult(GoogleSignInResult result) {
+        //If the login succeed
+        if (result.isSuccess()) {
+            //Getting google account
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            //Displaying name and email
+            textViewName.setText(acct.getDisplayName());
+            textViewEmail.setText(acct.getEmail());
+
+            //Initializing image loader
+            imageLoader = CustomVolleyRequest.getInstance(this.getApplicationContext())
+                    .getImageLoader();
+
+            try{
+
+                imageLoader.get(acct.getPhotoUrl().toString(),
+                        ImageLoader.getImageListener(profilePhoto,
+                                R.mipmap.ic_launcher,
+                                R.mipmap.ic_launcher));
+
+                profilePhoto.setVisibility(View.VISIBLE);
+                //Loading image
+                profilePhoto.setImageUrl(acct.getPhotoUrl().toString(), imageLoader);
+
+            }catch (Exception e){
+
+            }
+
+        } else {
+            //If login fails
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == signInButton) {
+            //Calling signin
+            signIn();
+        }else if(v==btnSignOut){
+            signOut();
+        }
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        updateUI(false);
+                    }
+                });
+    }
+
+    private void updateUI(boolean isSignedIn) {
+        if (isSignedIn) {
+            btnSignOut.setVisibility(View.VISIBLE);
+        } else {
+            btnSignOut.setVisibility(View.GONE);
+            profilePhoto.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
